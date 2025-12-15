c758476156729ed35034dfc6ecd9dd4314a7e8dc
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-15 21:57:45 +0600

    import icon

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-
+import {Code,Menu,X} from "lucide-react"
+import {NAV_LINKS ,PERSONAL_INFO } from "../../utils/constants"
 const Navbar = () => {
   useState();
   useEffect(() =>{
-    
+
   },[])
   return (
     <div>Navbar</div>
