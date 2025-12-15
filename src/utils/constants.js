e853612493eda4368b457582ea9c78202b509aba
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-15 21:35:20 +0600

    add NAV_LINKS

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    export const  NAV_LINKS = [
+        {  id:"about" , label:"About"},
+        {  id:"skills" , label:"Skills"},
+        {  id:"projects" , label:"Projects"},
+        {  id:"services" , label:"Services"},
+        {  id:"contact" , label:"Contact"}
+
+    ] 
