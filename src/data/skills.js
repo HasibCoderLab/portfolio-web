12b026d5a4aa8fec5c17454e1c698782f6da15b2
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-15 21:11:27 +0600

    add 3

---
+++ src/data/skills.js
@@ -0,0 +1,89 @@
+export const skills = [
+  {
+    id: 1,
+    name: "React.js",
+    icon: "Code2",
+    level: "Expert",
+    experiences:"Year"
+  },
+ {
+    id: 2,
+    name: "JavaScript",
+    icon: "FileCode",
+    level: "",
+    experiences:""
+  },,
+ {
+    id: 3,
+    name: "TypeScript",
+    icon: "FileCode",
+    level: "",
+    experiences:""
+  },
+  {
+    id: 4,
+    name: "Next.js",
+    icon: "Zap",
+    level: "",
+    experiences:""
+  },
+{
+    id: 5,
+    name: "Tailwind Css",
+    icon: "Palette",
+    level: "",
+    experiences:""
+  },
+  {
+    id: 6,
+    name: "Node.js",
+    icon: "Server",
+    level: "Intermideat",
+    experiences:""
+  },
+   {
+    id: 7,
+    name: "Git & Github",
+    icon: "GitBranch",
+    level: "",
+    experiences:""
+  },
+   {
+    id: 8,
+    name: "Redux",
+    icon: "DataBase",
+    level: "Intermideat",
+    experiences:""
+  },
+   {
+    id: 9,
+    name: "REST AIPs",
+    icon: "Globe",
+    level: "",
+    experiences:""
+  },
+   {
+    id: 10,
+    name: "Responsive Design",
+    icon: "SmartPhone",
+    level: "",
+    experiences:""
+  },
+   {
+    id: 11,
+    name: "Canva",
+    icon: "Canva",
+    level: "",
+    experiences:""
+  },
+   {
+    id: 12,
+    name: "Vite",
+    icon: "Rocket",
+    level: "",
+    experiences:""
+  },
+  
+  
+
+];
