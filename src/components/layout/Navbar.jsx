97b923192ed84258573a0403e791dc915565bfd3
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-15 22:02:09 +0600

    use var

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import {Code,Menu,X} from "lucide-react"
 import {NAV_LINKS ,PERSONAL_INFO } from "../../utils/constants"
 const Navbar = () => {
-  useState();
+   const [isOpenMenu , setIsOpenMenu] = useState(false);
+   const [isScrolled , setIsScrolled] = useState(false);
+const activeSections = useScrollSpy(NAV_LINKS.map(link => link.id));
   useEffect(() =>{
 
   },[])
