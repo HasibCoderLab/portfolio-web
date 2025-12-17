abd7451c56bbef8c593e8664b7d3926033973ccc
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-17 21:22:10 +0600

    add importance files

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {siReact,siNextdotjs,SiTailwindcss, siNodedotjs ,siMongodb } from "react-icons/si";
+import { PERSONAL_INFO,STATS } from "../../utils/constants";
+import { scrollToSection } from "../../hooks/useScrollSpy";
+import { FadeIn } from "../animations/FadeIn";
+import { RadialGradientBackground } from "../background/RadialGradientBackground";
 const Hero = () => {
+  useState
   return (
-    <div>Hero</div>
+    <div>
+    
+    </div>
   )
 }
 
