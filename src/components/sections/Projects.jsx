dcb7f92eed33997e3c610e1d0c0fe17cea59dc8c
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-20 15:22:07 +0600

    diclare States

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,9 +1,14 @@
-
 import { Briefcase ,  Sparkles , Target,Global,Palette ,Zap ,ChevronLeft,ChevronRight} from "lucide-react";
 import {projects,categories} from '../../data/projects'
 import FadeIn from "../animations/FadeIn";
 import ProjectCard from '../ui/ProjectCard'
+import { useState } from "react";
+import { useRef } from "react";
 const Projects = () => {
+    const [activeCatagory,setActiveCatagory] = useState('All');
+    const [currentIndex,setCurrentIndex] = useState(0);
+    const scrollContainerRef = useRef(null);
+
   return (
     <div>Projects</div>
   )
