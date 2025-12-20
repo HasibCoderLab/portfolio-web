8333784a97de99794fd2b2891584e4899606b27b
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-20 15:47:39 +0600

    for nextSlide

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,32 +1,52 @@
-import { Briefcase ,  Sparkles , Target,Global,Palette ,Zap ,ChevronLeft,ChevronRight} from "lucide-react";
-import {projects,categories} from '../../data/projects'
+import { Briefcase, Sparkles, Target, Global, Palette, Zap, ChevronLeft, ChevronRight } from "lucide-react";
+import { projects, categories } from '../../data/projects'
 import FadeIn from "../animations/FadeIn";
 import ProjectCard from '../ui/ProjectCard'
 import { useState } from "react";
 import { useRef } from "react";
 const Projects = () => {
-    const [activeCatagory,setActiveCatagory] = useState('All');
-    const [currentIndex,setCurrentIndex] = useState(0);
+    const [activeCategory, setActiveCategory] = useState('All');
+    const [currentIndex, setCurrentIndex] = useState(0);
     const scrollContainerRef = useRef(null);
 
-    const filteredProject = activeCatagory === 'All'
-    ?projects
-    :projects.filter(project => project.catagory === activeCatagory);
+    const filteredProjects = activeCatagory === 'All'
+        ? projects
+        : projects.filter(project => project.category === activeCatagory);
 
 
     // Reset carousel when catagory changes
 
-    const handleCatagoryChange =  (catagory) =>{
-        setActiveCatagory(catagory);
+    const handleCategoryChange = (category) => {
+        setActiveCategory(category);
         setCurrentIndex(0);
-        if(scrollContainerRef.current){
-                scrollContainerRef.current.scrollTo({left:0,behavior:'smoth'});
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollTo({ left: 0, behavior: 'smoth' });
         }
     }
 
-  return (
-    <div>Projects</div>
-  )
+    const scrollToIndex = (index) => {
+        setCurrentIndex(index);
+        if (scrollContainerRef.current) {
+            const container = scrollContainerRef.current;
+            const cardWidth = container.offsetWidth / 3;
+            container.scrollTo({
+                left: cardWidth * index,
+                behavior: 'smoth'
+            })
+        }
+    }
+
+
+    const nextSlide = () =>{
+        const maxIndex = Math.max(0,filteredProjects.length -3);
+        const newIndex = Math.min(currentIndex+1,maxIndex)
+    }
+
+
+
+    return (
+        <div>Projects</div>
+    )
 }
 
 export default Projects
